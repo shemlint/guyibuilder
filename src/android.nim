838207ee3bcf10104ca9d169764eca890f312974
TeddyShem <teddyshem100@gmail.com>
2022-04-16 11:09:14 +0300

    starting guyibuilder

---
+++ src/android.nim
@@ -0,0 +1,4 @@
+
+proc sum*(no1:int, no2:int):int=
+    no1 + no2
+
---
+++ src/main.nim
@@ -0,0 +1,8 @@
+import android
+
+echo("Working well")
+let a = stdin.readLine()
+echo("Entered : " & a)
+var n = android.sum(12,43)
+echo(n)
+
